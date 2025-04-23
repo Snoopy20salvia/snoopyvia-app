ff2974549906f6dcb5d1d49437784c3915db6d68
Snoopy20salvia <salvianoorc20@gmail.com>
2025-04-23 14:59:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("via's zone 🦦")
 st.write(
-    "bet you’ll remember my eyes even when you forget everything else (⁠≧⁠▽⁠≦⁠)"
+    "bet you’ll remember my eyes even when you forget everything else (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)"
 )
